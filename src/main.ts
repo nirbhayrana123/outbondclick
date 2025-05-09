9fb1248504ce613b961eb15d3543da1689264f55
nirbhayrana123 <nirbhydesign@gmail.com>
2025-05-09 21:13:20 +0530

    ommit changes

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as $ from 'jquery';
 (window as any).$ = $;
 (window as any).jQuery = $;
 import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.js';
 
 
