5412e5a99eedb0b0b95c4f97d2c65655fa528155
nirbhayrana123 <nirbhydesign@gmail.com>
2025-06-18 11:34:30 +0530

    Design fixes

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TestimonialComponent } from '../testimonial/testimonial.component';
 import { VideoSliderComponent } from '../video-slider/video-slider.component';
 import { FormComponent } from '../form/form.component';
 import { FaqComponent } from '../faq/faq.component';
 import { CircleProgressOptions, NgCircleProgressModule } from 'ng-circle-progress';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 @Component({
   selector: 'app-main-page',
   imports: [TestimonialComponent,VideoSliderComponent,FormComponent, FaqComponent,NgCircleProgressModule ],
@@ -25,5 +27,60 @@ import { CircleProgressOptions, NgCircleProgressModule } from 'ng-circle-progres
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+
+
+
+
+animateValues() {
+  this.animate('fromValue', 25, 50, 1);
+  // You can animate `toValue` or ROAS if needed.
+}
+
+animate(property: string, start: number, end: number, duration: number) {
+  const frameRate = 60;
+  const totalFrames = duration * 1000 / (1000 / frameRate);
+  let frame = 0;
+
+  const interval = setInterval(() => {
+    frame++;
+    const progress = frame / totalFrames;
+    (this as any)[property] = Math.floor(start + (end - start) * progress);
+    if (frame >= totalFrames) {
+      (this as any)[property] = end;
+      clearInterval(interval);
+    }
+  }, 1000 / frameRate);
+}
+
+
+dollarCounter = 0; // For $50 animation
+  leadCounter = 0;   // For 300+ leads animation
+  ngOnInit() {
+    this.animateDollarCounter(50, 1000); // Animate to $50 in 1 second
+    this.animateLeadCounter(300, 1000);  // Animate to 300 leads in 1 second
+  }
+  animateDollarCounter(target: number, duration: number) {
+    const steps = 50;
+    const increment = target / steps;
+    const timePerStep = duration / steps;
+    interval(timePerStep).pipe(
+      takeWhile(() => this.dollarCounter < target)
+    ).subscribe(() => {
+      this.dollarCounter = Math.min(this.dollarCounter + increment, target);
+      this.dollarCounter = parseFloat(this.dollarCounter.toFixed(2));
+    });
+  }
+  animateLeadCounter(target: number, duration: number) {
+    const steps = 50;
+    const increment = target / steps;
+    const timePerStep = duration / steps;
+    interval(timePerStep).pipe(
+      takeWhile(() => this.leadCounter < target)
+    ).subscribe(() => {
+      this.leadCounter = Math.ceil(this.leadCounter + increment);
+    });
+  }
+
+
 
 }
--- src/app/video-slider/video-slider.component.ts
+++ src/app/video-slider/video-slider.component.ts
@@ -19,23 +19,62 @@ export class VideoSliderComponent  {
     arrows: true,
     infinite: true,
     speed: 800,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     autoplay: false,
-  prevArrow: '<button type="button" class="slick-prev"><i class="bi bi-arrow-left"></i></button>',
-  nextArrow: '<button type="button" class="slick-next"><i class="bi bi-arrow-right"></i></button>'
+    prevArrow: '<button type="button" class="slick-prev"><i class="bi bi-arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="bi bi-arrow-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024, // tablet
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768, // mobile
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+
 
   videos = [
     {
-      src: 'https://development.revelatedesigns.com/wp-content/uploads/2024/10/Ripio-Hydration.mp4',
-      poster: 'https://development.revelatedesigns.com/wp-content/uploads/2024/10/Ripio-Hydration_thumb1.jpg'
+      src: 'https://www.youtube.com/embed/g_SvNVNb-nA',
+      poster: 'https://img.youtube.com/vi/g_SvNVNb-nA/maxresdefault.jpg'
     },
     {
-      src: 'https://development.revelatedesigns.com/wp-content/uploads/2024/10/Ripio-Zipper.mp4',
-      poster: 'https://development.revelatedesigns.com/wp-content/uploads/2024/10/Ripio-Zipper_thumb1.jpg'
+      src: 'https://www.youtube.com/embed/w0DwvAEvEos',
+      poster: 'https://img.youtube.com/vi/w0DwvAEvEos/maxresdefault.jpg'
+    },
+    {
+      src: 'https://www.youtube.com/embed/oEoNFHmpCT4',
+      poster: 'https://img.youtube.com/vi/oEoNFHmpCT4/maxresdefault.jpg'
+    },
+    {
+      src: 'https://www.youtube.com/embed/3YE9jQX4lFA',
+      poster: 'https://img.youtube.com/vi/3YE9jQX4lFA/maxresdefault.jpg'
     }
   ];
 
- 
+
+showModal = false;
+  currentVideo = '';
+
+  openVideo(videoUrl: string) {
+    this.currentVideo = videoUrl;
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.currentVideo = '';
+  }
+
 }
+
