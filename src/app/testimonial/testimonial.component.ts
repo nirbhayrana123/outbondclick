61fa6607116c2d354ef0cc1f48741b61eca6a7ad
nirbhayrana123 <nirbhydesign@gmail.com>
2025-07-23 10:23:51 +0530

    designfix

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -41,35 +41,30 @@ export class TestimonialComponent {
 
   testimonials = [
     {
-      text: "Great service and quality! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.",
-      name: "Jone",
-      role: "Designer",
-      img: "img/user.jpg"
+      text: "Govind was exceptional to work with. He is committed, dedicated, a fast learner, result driven, and he truly cares about providing the best results. He puts the client first, he has a great work ethic, he communicated efficiently throughout this project and he totally delivered. I very much enjoyed working with Govind and would not hesitate recommending him to anyone who is looking for high quality work.",
+      name: "Paul Money",
+      role: "Client",
+      img: "img/testimonials1.png"
     },
     {
-      text: "Loved working with the team. Nulla volutpat aliquam velit. Phasellus iaculis neque purus, euismod venenatis enim ultricies.",
-      name: "Emily",
-      role: "Developer",
-      img: "img/user.jpg"
+      text: "Absolutely amazing job! After 2 years of searching, we finally found someone who truly delivered well above our expectations! So easy work with, able to make calculated adjustment without direction and a remarkable attitude, Govind is our all-time favorite Upworks contractor. Understands paid ad campaigns on a very high level! I am not the easiest business owner to please, but was 150% satisfied with the experience with Govind! Highly recommen",
+      name: "Richard Force",
+      role: "Client",
+      img: "img/testimonials2.webp"
     },
     {
-      text: "Loved working with the team. Nulla volutpat aliquam velit. Phasellus iaculis neque purus, euismod venenatis enim ultricies.",
-      name: "Emily",
-      role: "Developer",
-      img: "img/user.jpg"
+      text: "Great to work with him. I already have him on the front my list for my next job in a couple of weeks.",
+      name: "Paolo",
+      role: "Client",
+      img: "img/testimonials3.png"
     },
     {
-      text: "Loved working with the team. Nulla volutpat aliquam velit. Phasellus iaculis neque purus, euismod venenatis enim ultricies.",
-      name: "Emily",
-      role: "Developer",
-      img: "img/user.jpg"
-    },
-    {
-      text: "Loved working with the team. Nulla volutpat aliquam velit. Phasellus iaculis neque purus, euismod venenatis enim ultricies.",
-      name: "Emily",
-      role: "Developer",
-      img: "img/user.jpg"
+      text: "It was really pleasure working with Mr. Sharma. He seems very knowledgeable and really took care of my project. Customer's satisfaction is his first priority. I will recommend him to everyone else. And I look forward to work again with him very soon.",
+      name: "Moritz Gruber",
+      role: "Client",
+      img: "img/testimonials5.webp"
     }
+    
   ];
 
   //  ngAfterViewInit(): void {
